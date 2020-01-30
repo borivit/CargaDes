64948dae452154871aab373bf987ac545903bc39
Borys <59554169+borivit@users.noreply.github.com>
2020-01-30 21:07:14 +0100

    Исправление ошибок

--- ejemplos/test_priem.php
+++ ejemplos/test_priem.php
@@ -3,6 +3,9 @@ define('ROOT_DIR', dirname(__FILE__));
 echo '<br>test: ';
 print_r($_FILES);
 echo '<br>' . str_repeat('-', 80) . '<br>';
+
+if (!is_dir(ROOT_DIR . '/test/')) mkdir(ROOT_DIR . '/test/', 0777);
+
 if (!empty($_FILES['file']) and !is_array($_FILES['file']['tmp_name'])) {
     move_uploaded_file($_FILES['file']['tmp_name'], ROOT_DIR . '/test/' . $_FILES['file']['name']);
 } elseif (!empty($_FILES['upload']) and !is_array($_FILES['upload']['tmp_name'])) {
